df53be33dad096686b669b65f026d1e152741b50
Vadim Tatarnikov <vadimtatarnikov98@gmail.com>
2022-03-01 16:17:26 +0300

    Finished second task

--- Lesson1.1/Lesson1.1/Program.cs
+++ Lesson1.1/Lesson1.1/Program.cs
@@ -10,6 +10,58 @@ namespace Lesson1._1
     {
         static void Main(string[] args)
         {
+
+            string star = " *";
+            string stars;
+
+            bool mainState = true;
+
+            while (mainState)
+            {
+                bool anotherState = true;
+
+                Console.WriteLine("Найдите площадь и периметр квадрата заданного квадрата.\n" +
+                "\nПожалуйста введите значение стороны квадрата\n");
+
+                int side = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nЗаданный квадрат:");
+
+                for (int i = 0; i < side; i++)
+                {
+                    stars = "";
+                    for (int j = 0; j < side; j++)
+                    {
+                        stars += star;
+                    }
+                    Console.WriteLine(stars);
+                }
+
+                Console.WriteLine("\nДля получения ответов клавишу нажмите Enter");
+                Console.ReadLine();
+
+                Console.WriteLine("Ответы:\n");
+                Console.WriteLine("Площадь квадрата равна: " + Math.Pow(side, 2));
+                Console.WriteLine("Периметр квадрата равен: " + side * 4);
+
+                while (anotherState)
+                {
+                    Console.WriteLine("\nХотите попробывать решить еще раз? (Да/Нет)");
+                    string answer = Console.ReadLine();
+                    Console.Clear();
+
+                    if (answer == "Нет")
+                    {
+                        anotherState = false;
+                        mainState = false;
+                    }
+                    else if (answer == "Да")
+                    {
+                        anotherState = false;
+                    }
+                }
+            }
         }
     }
 }
+
