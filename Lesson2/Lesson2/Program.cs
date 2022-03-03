539bfa5e1ee9988b6038679dd8556945457424a7
Vadim Tatarnikov <vadimtatarnikov98@gmail.com>
2022-03-03 16:45:57 +0300

    Finished homework

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -9,41 +9,87 @@ namespace Lesson2
     internal class Program
     {
         static void Main(string[] args)
-        /*
-        1) Посчитать сумму всех трех цифр трехзначного числа, заданного константой (const). Вывести сумму на печать.
-        Например:
-        735 = 15 (7 + 3 + 5)
-
-
-        2) Ввести с клавиатуры координаты выстрела x1 и y1. Записать в bool переменную логическое выражение, которое будет истинным (true) тогда и только тогда, когда этот выстрел попадает в круглую мишень (точка лежит внутри круга радиусом R). Вывести bool переменную на экран.
-        Формула круга: x^2 + y^2 = R^2. Следовательно, условие нахождения точки ВНУТРИ круга будет... (подумайте о знаке сравнения). 
-        Запись x^2 означает "икс в квадрате". 
-
-
-        3) Пользователь вводить трехзначное число. Записать логическое условие, которые будет истинным тогда и только тогда, когда его средняя цифра меньше или равна (<=) первой (левой) и одновременно строго больше (>) последней (правой).
-        Например: 
-        742 (true)
-        773 (true)
-        755 (false)
-        354 (false)
-        335 (false)
-
-
-        4) Ввести целое трехзначное число. Поменять местами его первую и последнюю цифры. Собрать новое число в новой переменной!
-        Например:
-        365 -> 563
-        947 -> 749
-        Вспоминаем арифметику... 365 это 3*100 + 6*10 + 5 :)
-
-
-        5) Записать логическое условие, которое проверит, что в double числе нулевая дробная часть.
-        Например:
-        156.0 (true)
-        156.2 (false)
-        Подсказка: сравните само число double number и его явное приведение к (int)number
-         */
         {
+            const int ten = 10;
+            const int hundred = 100;
 
+            // Task 1 //
+            Console.WriteLine("Task 1");
+
+            const int numForFirstTask = 123;
+
+            int firstNumberForFirstTask = numForFirstTask / hundred;
+            int secondNumberForFirstTask = numForFirstTask / ten - firstNumberForFirstTask * ten;
+            int thirdNumberForFirstTask = numForFirstTask - firstNumberForFirstTask * hundred - secondNumberForFirstTask * ten;
+            int resultForFirstTask = firstNumberForFirstTask + secondNumberForFirstTask + thirdNumberForFirstTask;
+            Console.WriteLine($"The sum of the digits of the number {numForFirstTask} is: {resultForFirstTask}");
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+
+            // Task 2 //
+            Console.WriteLine("Task 2");
+
+            Console.Write("Please write x1: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Please write y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Please write radius: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            bool stateForSecondTask = Math.Pow(x1, 2) + Math.Pow(x1, 2) < Math.Pow(radius, 2);
+
+            Console.Write("Result:");
+            Console.WriteLine(stateForSecondTask);
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+
+            // Task 3 //
+            Console.WriteLine("Task 3");
+
+            Console.Write("Please write number: ");
+            int numForSecondTask = int.Parse(Console.ReadLine());
+
+            int firstNumberForThirdTask = numForSecondTask / hundred;
+            int secondNumberForThirdTask = numForSecondTask / ten - firstNumberForThirdTask * ten;
+            int thirdNumberForThirdTask = numForSecondTask - firstNumberForThirdTask * hundred - secondNumberForThirdTask * ten;
+
+            bool stateForThirdTask = secondNumberForThirdTask <= firstNumberForThirdTask && secondNumberForThirdTask > thirdNumberForThirdTask;
+
+            Console.Write("Result:");
+            Console.WriteLine(stateForThirdTask);
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+
+            // Task 4 //
+            Console.WriteLine("Task 4");
+
+            Console.Write("Please write number: ");
+            int numberForForthTask = int.Parse(Console.ReadLine());
+
+            int firstNumberForForthTask = numberForForthTask / hundred;
+            int secondNumberForForthTask = numberForForthTask / ten - firstNumberForForthTask * ten;
+            int thirdNumberForForthTask = numberForForthTask - firstNumberForForthTask * hundred - secondNumberForForthTask * ten;
+
+            int resultForForthTask = thirdNumberForForthTask * hundred + secondNumberForForthTask * ten + firstNumberForForthTask;
+
+            Console.Write("Result:");
+            Console.WriteLine(resultForForthTask);
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+
+            // Task 5 //
+            Console.WriteLine("Task 5");
+
+            Console.Write("Please write number: ");
+            double numberForFifthTask = double.Parse(Console.ReadLine());
+            int temp = (int)numberForFifthTask;
+
+            bool stateForFifthTask = numberForFifthTask % temp == 0;
+
+            Console.Write("Result:");
+            Console.WriteLine(stateForFifthTask);
+            Console.WriteLine("Please press Enter to end.");
+            Console.ReadLine();
         }
     }
 }
