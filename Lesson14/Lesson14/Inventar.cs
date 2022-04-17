e5b99370f65686153fa50a13e95972ca292e49dc
Vadim Tatarnikov <vadimtatarnikov98@gmail.com>
2022-04-18 00:52:27 +0300

    Added lesson14

---
+++ Lesson14/Lesson14/Inventar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14
+{
+    internal class Inventar
+    {
+        private string _owner = "Unknown";
+        private double _size = 0;
+        private double _maxSize = 500;
+
+        public string Owner { get { return _owner; } set { _owner = value.Trim(); } }
+        public double Size { get { return _size; } set { _size = value; } }
+        public double MaxSize { get { return _maxSize; } set { _maxSize = value; } }
+
+        LinkedList<Item> Items = new LinkedList<Item>();
+
+        public Inventar(string owner, double size, double maxSize)
+        {
+            Owner = owner;
+            Size = size;
+            MaxSize = maxSize;
+            Items = new LinkedList<Item>();
+        }
+    }
+}
---
+++ Lesson14/Lesson14/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14
+{
+    enum ItemType{ Helm, Armor, Gloves, Boots, Weapon, Shield, Belt };
+
+    internal class Item
+    {
+        private string _name = "Uknown";
+        private double _cost = 25;
+        private double _weigth = 30;
+        public ItemType Type { get; set; }
+        public string Name { get { return _name; } set { _name = value.Trim(); } }
+        public double Cost { get { return _cost; } set { _cost = value; } }
+        public double Weigth { get { return _weigth; } set { _weigth = value; } }
+
+        public Item(string name, double cost, double weight, ItemType type)
+        {
+            Name = name;
+            Cost = cost;
+            Weigth = weight;
+            Type = type;
+        }
+    }
+}
